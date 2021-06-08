35db0fc07a6e3f9f715e8a698e3b4c1f4f83a466
Veronika Stanovská <veronika.stanovska@kosickaakademia.sk>
2021-06-08 12:21:10 +0200

    hello world 2

--- Main.java
+++ Main.java
@@ -2,6 +2,10 @@ package git;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        if (1>0){
+            System.out.println("Hello World");
+        }
+
+
     }
 }
